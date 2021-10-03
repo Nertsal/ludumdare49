93ab66b22024b759a850486ffe31a0f8659e573e
Nertsal <Nertsal@users.noreply.github.com>
2021-10-03 14:37:53 +0000

    deploy: bc492d9aa6f6aa26dd3fb292adc46f91a53f949e

--- ludumdare49.js
+++ ludumdare49.js
@@ -849,7 +849,7 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper222 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper221 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 29, __wbg_adapter_26);
         return addHeapObject(ret);
     };
